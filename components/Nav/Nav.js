ef5b404b512df0de3683ef63849b0046c524be06
Raphael do Amarante Gomes <raphaag10@gmail.com>
2026-05-09 14:31:15 -0300

    Add nova navbar

--- components/Nav/Nav.js
+++ components/Nav/Nav.js
@@ -4,31 +4,35 @@ export async function Nav() {
   const user = authStore.getCurrentUser();
 
   return `
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-      <div class="container">
-        <a class="navbar-brand" href="#/">Meu Site</a>
+    <nav class="navbar navbar-expand-lg bg-nav ">
+  <div class="container ">
+    <a class="navbar-brand logo" href="#/"> <img src="/images/logo.png" alt="Logo"></a>
 
-        <button
-          class="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarMenu"
-        >
-          <span class="navbar-toggler-icon"></span>
-        </button>
+    <!-- Botão hamburguer para mobile -->
+    <button
+      class="navbar-toggler"
+      type="button"
+      data-bs-toggle="collapse"
+      data-bs-target="#navbarMenu"
+    >
+      <span class="navbar-toggler-icon"></span>
+    </button>
 
-        <div class="collapse navbar-collapse" id="navbarMenu">
-          <ul class="navbar-nav ms-auto">
-            <li class="nav-item">
-              <a class="nav-link" href="#/">Home</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#/sobre">Sobre</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#/recompensas">Recompensas</a>
-            </li>
-
+    <!-- Links -->
+    <div class="collapse navbar-collapse  " id="navbarMenu">
+      <ul class="navbar-nav ms-auto poppins-semibold ">
+        <li class="nav-item">
+          <a class="nav-link" href="#/">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#/clinicas">Clínicas</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#/careplus">CarePlus+</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#/planoseprodutos">Planos e Produtos</a>
+        </li>
             ${
               user
                 ? `
